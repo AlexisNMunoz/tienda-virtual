2e164791966bf15a6f5c12470423e9bc6846ca1c
AlexisNMunoz <alexisnahuelmunioz@gmail.com>
2024-05-27 00:24:04 -0300

    fixed video

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,22 +1,19 @@
 export default function IndexPage() {
   return (
-    <section className=''>
-      <div className='relative flex w-full h-screen items-center'>
-        <div className=' w-full md:h-screen overflow-hidden bg-slate-700 aspect-square pt-24 hidden md:block'>
-          <video
-            autoPlay
-            muted
-            playsInline
-            loop
-            className='opacity-50 z-0 '
-          >
-            <source
-              src='hero.mp4'
-              type='video/mp4'
-              className=''
-            />
-          </video>
-        </div>
+    <section>
+      <div className='w-full overflow-hidden pt-20 hidden md:block bg-slate-800'>
+        <video
+          autoPlay
+          muted
+          playsInline
+          loop
+          className='opacity-50 '
+        >
+          <source
+            src='hero2.mp4'
+            type='video/mp4'
+          />
+        </video>
       </div>
     </section>
   )
