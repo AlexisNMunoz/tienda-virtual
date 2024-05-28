9d9b9bc96731559828c3e07ae4974fdb6fd493b6
AlexisNMunoz <alexisnahuelmunioz@gmail.com>
2024-05-28 14:09:49 -0300

    update index

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -13,8 +13,9 @@ export default function IndexPage() {
   return (
     <>
       <div className=' relative flex justify-center items-end'>
-        <div className='absolute z-10 py-10'>
+        <div className='absolute z-10 py-10 justify-center flex flex-col gap-3'>
           <h2 className='  font-black uppercase text-6xl italic text-rose-500'>Lonely</h2>
+          <button className=' text-white underline underline-offset-4 text-sm'>Ver más</button>
         </div>
         <div className='w-full overflow-hidden pt-20 hidden md:block bg-slate-800 z-0'>
           <video
