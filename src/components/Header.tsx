38006ebd4a116f938e94aa1701a5e153c73b2f38
AlexisNMunoz <alexisnahuelmunioz@gmail.com>
2024-05-28 13:47:58 -0300

    add categories in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link, NavLink } from 'react-router-dom'
+import { useAppStore } from '../store/useAppStore'
 import {
   Bars3Icon,
   MagnifyingGlassIcon,
@@ -7,9 +8,11 @@ import {
 } from '@heroicons/react/24/outline'
 
 export default function Header() {
+  const categories = useAppStore((state) => state.categories)
+
   return (
     <header className='bg-white shadow-md fixed top-0 left-0 right-0 z-20'>
-      <div className='container flex justify-between mx-auto items-center gap-4 py-8 w-[90%] '>
+      <div className='container flex justify-between mx-auto items-center gap-4 py-8 w-[90%]'>
         <div className='flex md:gap-8 gap-5 items-center'>
           <button>
             <Bars3Icon className='w-6 h-6 text-gray-600' />
@@ -42,6 +45,16 @@ export default function Header() {
             <span className='hidden md:block'>Carrito</span>
           </NavLink>
         </nav>
+      </div>
+      <div className='w-full container justify-evenly mx-auto pb-3 hidden md:flex'>
+        {categories.map((category) => (
+          <button
+            key={category}
+            className='capitalize text-slate-600'
+          >
+            {category}
+          </button>
+        ))}
       </div>
     </header>
   )
