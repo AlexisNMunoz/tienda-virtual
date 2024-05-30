7e10b9628f10a3a0c6f430d44779d650f3567805
AlexisNMunoz <alexisnahuelmunioz@gmail.com>
2024-05-30 00:38:42 -0300

    add product and animation useMemo

--- src/components/Productos.tsx
+++ src/components/Productos.tsx
@@ -1,22 +1,36 @@
+import { useEffect, useMemo } from 'react'
 import { useAppStore } from '../store/useAppStore'
 import ProductCard from './ProductCard'
+import SkeletonAnimation from './SkeletonAnimation'
 
 export default function Productos() {
   const products = useAppStore((state) => state.products)
+  const fetchProducts = useAppStore((state) => state.fetchProducts)
+
+  const hasProducts = useMemo(() => products.length, [products])
+
+  useEffect(() => {
+    fetchProducts()
+  }, [fetchProducts])
 
   return (
     <>
       <h2 className='py-16 text-4xl italic text-center text-slate-600  mt-24'>
         Explore la Selección de las Creaciones
       </h2>
-      <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 container mx-auto w-[90%] content-center gap-4 py-5'>
-        {products.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-          />
-        ))}
-      </section>
+
+      {hasProducts ? (
+        <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 container mx-auto w-[90%] content-center gap-4 py-5'>
+          {products.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+            />
+          ))}
+        </section>
+      ) : (
+        <SkeletonAnimation />
+      )}
     </>
   )
 }
