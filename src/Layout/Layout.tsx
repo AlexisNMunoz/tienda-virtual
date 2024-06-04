0b0fd235e5c67c8b6805eeb769c4b1eb0fff1a89
AlexisNMunoz <alexisnahuelmunioz@gmail.com>
2024-06-03 22:41:47 -0300

    add localStorage cart

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -5,14 +5,16 @@ import Header from '../components/Header'
 
 export default function Layout() {
   const loadFromStorage = useAppStore((state) => state.loadFromStorage)
+  const loadFromStorageCart = useAppStore((state) => state.loadFromStorageCart)
   const cart = useAppStore((state) => state.cart)
 
   const hasCart = useMemo(() => cart.length, [cart])
 
   useEffect(() => {
     loadFromStorage()
+    loadFromStorageCart()
     hasCart
-  }, [hasCart, loadFromStorage])
+  }, [hasCart, loadFromStorage, loadFromStorageCart])
   return (
     <>
       <Header />
--- src/store/cartShopSlice.ts
+++ src/store/cartShopSlice.ts
@@ -5,6 +5,7 @@ export type CartSliceType = {
   cart: Product[]
   handleClickCart: (product: Product) => void
   cartExist: (id: Product['id']) => boolean
+  loadFromStorageCart: () => void
 }
 
 export const createCartSlice: StateCreator<CartSliceType> = (set, get) => ({
@@ -19,8 +20,17 @@ export const createCartSlice: StateCreator<CartSliceType> = (set, get) => ({
         cart: [...get().cart, product]
       })
     }
+    localStorage.setItem('cart', JSON.stringify(get().cart))
   },
   cartExist: (id) => {
     return get().cart.some((product) => product.id === id)
+  },
+  loadFromStorageCart: () => {
+    const storedCart = localStorage.getItem('cart')
+    if (storedCart) {
+      set({
+        cart: JSON.parse(storedCart)
+      })
+    }
   }
 })
